54b64aa8470f41f1ef1d680da96f87f37f647fc5
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-26 18:59:28 +0500

    created home slides in models

--- backend/models.py
+++ backend/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class HomeSlides(models.Model):
+    class Meta():
+        verbose_name = 'Slide'
+        verbose_name_plural = 'Home Slides'
+    
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    image = models.ImageField()
