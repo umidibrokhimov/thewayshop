f51b78105941d5793bf30c896abdccfd6a3bf42e
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-26 16:56:51 +0500

    created about section

--- backend/urls.py
+++ backend/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('about/', About.as_view(), name='about'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = 'index.html'
+
+class About(TemplateView):
+    template_name = 'about.html'
