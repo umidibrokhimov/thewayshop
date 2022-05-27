c8a6c368f8a756f1c02e6d0c802c5c547889763c
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-27 10:40:17 +0500

    created some categories

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.db import models
 
 class HomeSlides(models.Model):
@@ -35,7 +36,42 @@ class TeamMembers(models.Model):
     profession = models.CharField(max_length=20)
     about = models.TextField()
     image = models.ImageField()
-    fb_account = models.URLField(max_length=10, null=True, blank=True)
-    tr_account = models.URLField(max_length=10, null=True, blank=True)
-    gl_account = models.URLField(max_length=10, null=True, blank=True)
-    yt_account = models.URLField(max_length=10, null=True, blank=True)
+    fb_account = models.URLField(max_length=20, null=True, blank=True)
+    tr_account = models.URLField(max_length=20, null=True, blank=True)
+    gl_account = models.URLField(max_length=20, null=True, blank=True)
+    yt_account = models.URLField(max_length=20, null=True, blank=True)
+
+# Products model
+class Products(models.Model):
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+    
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    image = models.ImageField()
+    price = models.IntegerField()
+
+# Categories model
+class Categories(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
+    name = models.CharField(max_length=20)
+
+# Products categories model
+class ProductCategories(models.Model):
+    class Meta:
+        verbose_name = 'Product Category'
+        verbose_name_plural = 'Product Categories'
+    
+    name = models.CharField(max_length=20)
+
+# Brand categories
+class BrandCategories(models.Model):
+    class Meta:
+        verbose_name = 'Brand Category'
+        verbose_name_plural = 'Brand Categories'
+    
+    name = models.CharField(max_length=20)
