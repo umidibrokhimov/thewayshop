4745c7793bf388be285a77dd0f3c4460948e6879
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-27 19:22:14 +0500

    some changes in template location

--- backend/models.py
+++ backend/models.py
@@ -77,6 +77,7 @@ class Categories(models.Model):
         verbose_name_plural = 'Categories'
     
     name = models.CharField(max_length=20)
+    image = models.ImageField()
 
     def __str__(self):
         return self.name
--- thewayshop/settings.py
+++ thewayshop/settings.py
@@ -104,11 +104,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/assets/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static/'
+    BASE_DIR / 'static/assets/'
 ]
+
+MEDIA_ROOT = 'static/media/'
+
+MEDIA_URL = 'static/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
