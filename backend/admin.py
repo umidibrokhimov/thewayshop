c078e62e1aa8f53757ba637c2a92c875f43f6878
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-27 18:11:02 +0500

    added models for admin

--- backend/admin.py
+++ backend/admin.py
@@ -3,3 +3,8 @@ from .models import *
 
 # Register your models here.
 admin.site.register(HomeSlides)
+admin.site.register(HomeCategories)
+admin.site.register(Blog)
+admin.site.register(Products)
+admin.site.register(Categories)
+admin.site.register(BrandCategories)
