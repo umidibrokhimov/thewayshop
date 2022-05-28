397da8c861fae40de4af19a9380de8e800de4733
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-28 14:36:16 +0500

    added home slides in frontend

--- backend/views.py
+++ backend/views.py
@@ -7,6 +7,16 @@ class Home(ListView):
     queryset = Categories.objects.all()
     template_name = 'index.html'
 
+    def get_queryset(self):
+        return HomeSlides.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        # here we can add so many context using that way
+        context['slides'] = HomeSlides.objects.all()
+        return context
+
+
 class About(ListView):
     context_object_name = 'teammembers'
     queryset = TeamMembers.objects.all()
