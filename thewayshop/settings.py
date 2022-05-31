8decf7631fe88f230ac3f0903fef5aecf66cd991
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-05-31 10:07:33 +0500

    created login

--- thewayshop/settings.py
+++ thewayshop/settings.py
@@ -118,3 +118,5 @@ MEDIA_URL = 'static/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
--- thewayshop/urls.py
+++ thewayshop/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static 
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(), name='login'),
     path('', include('backend.urls'))
 ]
 
