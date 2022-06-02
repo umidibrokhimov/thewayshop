dab2ef148c23debae1dd938940826f47e4da0020
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-06-02 18:43:59 +0500

    initial committ

--- thewayshop/settings.py
+++ thewayshop/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=x521#_+*kz8s9q*92%vt=qrpl#0*_0b52@uk@%)p@r0s!rl$d'
+SECRET_KEY = '521#_+*kz8s9q*92%vt=qrpl#0*_0b52@uk@%)p@r0s!rl$d'
 
 
 
