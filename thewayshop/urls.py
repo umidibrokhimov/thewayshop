86d1ad6e53520eea811cad6b1988af18b2305386
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-06-14 14:45:03 +0500

    created prefix to urls

--- thewayshop/urls.py
+++ thewayshop/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('signup/', RegistrationView.as_view(), name='signup'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + i18n_patterns(
     path('i18n/', include('django.conf.urls.i18n')),
-    path('', include('backend.urls'))
+    path('', include('backend.urls')),
+    prefix_default_language=False
 )
