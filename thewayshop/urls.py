4b8e97492e51733fad26214709c602769e33bf9c
Umidjon Ibrokhimov <u.ibrokhimov1@gmail.com>
2022-06-14 14:23:10 +0500

    added i18n details to project

--- thewayshop/urls.py
+++ thewayshop/urls.py
@@ -4,11 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static 
 from django.contrib.auth.views import LoginView, LogoutView
 from backend.views import *
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', RegistrationView.as_view(), name='signup'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + i18n_patterns(
     path('', include('backend.urls'))
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
